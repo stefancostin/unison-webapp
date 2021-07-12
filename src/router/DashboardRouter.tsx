ec851b8eb6dfe9ffcf6153bc3ca79c7577465ff9
Stefan Costin <stefanfcostin@gmail.com>
2021-07-12 09:44:46 +0300

    Fix agent control page routing

--- src/router/DashboardRouter.tsx
+++ src/router/DashboardRouter.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 import { DashboardRoutes } from './routes';
+import AgentControlPage from 'modules/agents/agent-control-page';
 import AgentsPage from 'modules/agents/agents-page';
-import EntitiesPage from 'modules/entities-page';
+import EntitiesPage from 'modules/entities/entities-page';
 import LogsPage from 'modules/logs-page';
-import NodesPage from 'modules/nodes/nodes-page';
 import NodeControlPage from 'modules/nodes/node-control-page';
+import NodesPage from 'modules/nodes/nodes-page';
 
 const DashboardRouter = (): JSX.Element => {
   let { path, url } = useRouteMatch();
 
   return (
     <Switch>
+      <Route path={[`${path}/${DashboardRoutes.Agents}/add`, `${path}/${DashboardRoutes.Agents}/:id/edit`]}>
+        <AgentControlPage />
+      </Route>
       <Route path={`${path}/${DashboardRoutes.Agents}`}>
         <AgentsPage />
       </Route>
@@ -21,10 +25,7 @@ const DashboardRouter = (): JSX.Element => {
       <Route path={`${path}/${DashboardRoutes.Logs}`}>
         <LogsPage />
       </Route>
-      <Route path={`${path}/${DashboardRoutes.Nodes}/add`}>
-        <NodeControlPage />
-      </Route>
-      <Route path={`${path}/${DashboardRoutes.Nodes}/:id/edit`}>
+      <Route path={[`${path}/${DashboardRoutes.Nodes}/add`, `${path}/${DashboardRoutes.Nodes}/:id/edit`]}>
         <NodeControlPage />
       </Route>
       <Route path={`${path}/${DashboardRoutes.Nodes}`}>
