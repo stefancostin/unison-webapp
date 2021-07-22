7b22603b560e8eae9ff0f4a6204a49cff58fea05
Stefan Costin <stefanfcostin@gmail.com>
2021-07-22 19:07:52 +0300

    Add route for login page

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserHistory } from 'history';
 import { Router as ReactRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from 'modules/dashboard';
+import LoginPage from 'modules/login';
 import ErrorPage from 'modules/error-page';
 
 export const history = createBrowserHistory();
@@ -10,6 +11,9 @@ const Router = (): JSX.Element => (
     <Switch>
       <Route path="/dashboard">
         <Dashboard />
+      </Route>
+      <Route path="/login">
+        <LoginPage />
       </Route>
       <Route path="*">
         <ErrorPage title="404" status="404" />
