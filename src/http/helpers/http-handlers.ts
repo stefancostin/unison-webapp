978435361c43a412073e1a3867d3ee89ff317599
Stefan Costin <stefanfcostin@gmail.com>
2021-07-27 07:16:20 +0300

    Add null check in authorization error handler

--- src/http/helpers/http-handlers.ts
+++ src/http/helpers/http-handlers.ts
@@ -1,3 +1,4 @@
+import { isEmpty } from 'lodash';
 import { AxiosError, AxiosResponse } from 'axios';
 import { history } from 'router/Router';
 import { GeneralRoutes } from 'router/routes';
@@ -10,6 +11,9 @@ const Forbidden: number = 403;
 export const handleResponse = <T>(response: AxiosResponse): T => response.data;
 
 export const handleUnauthorized = (err: AxiosError): void => {
+  if (isEmpty(err) || isEmpty(err.response)) {
+    return;
+  }
   if (err.response.status === Unauthorized || err.response.status === Forbidden) {
     history.push(`/${GeneralRoutes.Login}`);
   }
