de5658c007e5a0fc1a8d66494f7a912836ba0b77
Stefan Costin <stefanfcostin@gmail.com>
2021-09-04 14:32:52 +0300

    Change notification icons

--- src/services/notification-service.tsx
+++ src/services/notification-service.tsx
@@ -1,11 +1,11 @@
 import { notification } from 'antd';
-import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
 const getIcon = (success: boolean): JSX.Element => {
   if (success) {
-    return <SmileOutlined style={{ color: '#108ee9' }} />;
+    return <CheckCircleOutlined style={{ color: '#108ee9' }} />;
   }
-  return <FrownOutlined style={{ color: '#ff0000' }} />;
+  return <CloseCircleOutlined style={{ color: '#ff0000' }} />;
 };
 
 const sendNotification = (message: string, description: string, success: boolean = true): void => {
